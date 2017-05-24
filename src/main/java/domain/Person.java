c66f3fae1668a9b40c8f01aa0d5b4f3ddfe57b7f
FaerieRose <rosalynn5772-git@yahoo.com>
2017-05-24 16:38:31 +0200

    Config files and domain/Person.java added

---
+++ src/main/java/domain/Person.java
@@ -0,0 +1,59 @@
+package domain;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass
+public class Person {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+	
+	@Column(nullable=false)
+	private String firstname;
+	@Column(nullable=false)
+	private String surname;
+	@Column(unique=true, nullable=false)
+	private String username;
+	private String password;
+
+	/* =================================================================== */
+	/* Getters & Setters                                                   */ 
+	/* =================================================================== */
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	/* =================================================================== */
+	public String getFirstname() {
+		return firstname;
+	}
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+	/* =================================================================== */
+	public String getSurname() {
+		return surname;
+	}
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+	/* =================================================================== */
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	/* =================================================================== */
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	
+}
